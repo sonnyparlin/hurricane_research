fbe7455c7476bbfed8abb007ae0cf9a0e48624c1
Sonny Parlin <sonnyparlin@sonnys-mbp.home>
2018-09-10 21:44:40 -0400

    .

--- hurricane.py
+++ hurricane.py
@@ -5,11 +5,6 @@ from bs4 import BeautifulSoup
 import re
 
 def simple_get(url):
-    """
-    Attempts to get the content at `url` by making an HTTP GET request.
-    If the content-type of response is some kind of HTML/XML, return the
-    text content, otherwise return None.
-    """
     try:
         with closing(get(url, stream=True)) as resp:
             if is_good_response(resp):
@@ -23,9 +18,6 @@ def simple_get(url):
 
 
 def is_good_response(resp):
-    """
-    Returns True if the response seems to be HTML, False otherwise.
-    """
     content_type = resp.headers['Content-Type'].lower()
     return (resp.status_code == 200
             and content_type is not None
@@ -33,15 +25,49 @@ def is_good_response(resp):
 
 
 def log_error(e):
-    """
-    It is always a good idea to log errors.
-    This function just prints them, but you can
-    make it do anything.
-    """
     print(e)
+
+def build_year_str(raw_str):
+    return raw_str[0:4]
+
+def build_month_str(raw_str):
+    month=raw_str[5:9]
+    mo=re.sub("[\t\s]","",month)
+    mo=re.sub("Sp-O","Sep",mo)
+    mo=re.sub("Jl-A","Jul",mo)
+    return mo
     
+def build_state_str(raw_str):
+    state=raw_str[9:15]
+    st=re.sub("[\t\*\,\&\#\s+\;0-9a-z]","",state)
+    st=re.sub("LA","AL",st)
+    st=st[0:2]
+    return st
     
-def split_data(data):
+def collect_data(raw_str):
+    cat=raw_str[9:]
+    cat = re.sub(r"\t","_",cat).strip()
+    tokens = cat.split("_")
+    str_list = filter(None, tokens)
+    cat = str_list[-4].strip()
+    cat = re.sub("[\D]","",cat)
+    if cat == "":
+        cat="TS"
+        
+    name = str_list[-1]
+    name = re.sub("[-\"\s]","",name)
+    pressure = str_list[-3]
+    max_winds = str_list[-2]
+    
+    if len(pressure) == 1:
+        pressure = max_winds
+        max_winds = '-----'    
+    pressure = re.sub("[-]","",pressure)
+    max_winds = re.sub("[-]","",max_winds)
+    
+    return cat,name,pressure,max_winds
+    
+def tokenize_and_collect(data):
     myList = data.splitlines()
     
     myfile = open('Hurricane.txt', 'w')        
@@ -50,39 +76,17 @@ def split_data(data):
         pattern = re.compile("^([0-9]{4})")
         if pattern.match(item_string):
             item=re.sub("[\*]"," ",item_string)
-            year=item[0:4]
-            month=item[5:9]
-            mo=re.sub("[\t\s]","",month)
-            mo=re.sub("Sp-O","Sep",mo)
-            mo=re.sub("Jl-A","Jul",mo)
-            if (mo == '-No'):
+            year=build_year_str(item)
+            month=build_month_str(item)
+            if (month == '-No'):
                 continue
-            state=item[9:15]
-            st=re.sub("[\t\*\,\&\#\s+\;0-9a-z]","",state)
-            st=re.sub("LA","AL",st)
-            st=st[0:2]
-            cat=item[9:]
-            cat = re.sub(r"\t","_",cat).strip()
-            tokens = cat.split("_")
-            str_list = filter(None, tokens)
-            cat = str_list[-4].strip()
-            cat = re.sub("[\D]","",cat)
-            if cat == "":
-                cat="TS"
-            name = str_list[-1]
-            name = re.sub("[-\"\s]","",name)
-            pressure = str_list[-3]
-            max_winds = str_list[-2]
-            if len(pressure) == 1:
-                pressure = max_winds
-                max_winds = '-----'    
-            pressure = re.sub("[-]","",pressure)
-            max_winds = re.sub("[-]","",max_winds)
+            state=build_state_str(item)
+            cat,name,pressure,max_winds=collect_data(item)
             
-            myfile.write("{0},{1},{2},{3},{4},{5},{6}\n".format(year, mo, st,cat,pressure,max_winds,name))
-            #print(year, mo, st,cat,pressure,max_winds,name)
+            myfile.write("{0},{1},{2},{3},{4},{5},{6}\n".format(year,month,state,cat,pressure,max_winds,name))
+            print(year,month,state,cat,pressure,max_winds,name)
         
     myfile.close()
 
 raw_html = simple_get('http://www.aoml.noaa.gov/hrd/hurdat/All_U.S._Hurricanes-aug2011.html')
-split_data((raw_html))
+tokenize_and_collect((raw_html))
